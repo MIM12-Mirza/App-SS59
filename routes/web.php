1eb717f0f59efffc6051886feccc06ba054d2bda
MIM12-Mirza <mikramulmirza012@gmail.com>
2022-12-19 12:02:50 +0700

    Tampilan dan Proses Login

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.master');
-});//-> middleware('auth');
+})-> middleware('auth');
 
 Auth::routes();
 
