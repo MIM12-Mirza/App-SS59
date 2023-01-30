12f8c0869812d6d574d1c41161b2a07ace619042
MIM12-Mirza <mikramulmirza012@gmail.com>
2023-01-30 10:10:11 +0700

    view index jurusan

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class JabatanController extends Controller
+class MahasiswaController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class JabatanController extends Controller
      */
     public function index()
     {
-        return view('jabatan.index');
+        //
     }
 
     /**
